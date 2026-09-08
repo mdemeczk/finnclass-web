517a7f1e7555e50215087a714d1dc96eb2376e12
mdemeczk <demeczky.mihaly@gmail.com>
2026-09-08 10:42:40 +0200

    minden site on azonos megjelenes az npm run build, npm run dev, es a https://mdemeczk.github.io/finnclass-web/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 export default defineConfig({
-  site: 'https://mdemeczk.github.io',
-  base: '/',
+  site: isGitHubPages ? 'https://mdemeczk.github.io' : 'http://new.finnclass.hu',
+  base: isGitHubPages ? '/finnclass-web' : '/',
 });
